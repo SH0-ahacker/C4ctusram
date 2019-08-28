e6f164a1e4602b69ed0c10030827f8b176c92f89
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-08-28 20:35:12 -0300

    Create .c

---
+++ resurces/C/cursor/.c
@@ -0,0 +1,10 @@
+PAYLOADFUNCTIONDEFAULT(payloadCursor) {
+	PAYLOADHEAD
+
+	POINT cursor;
+	GetCursorPos(&cursor);
+
+	SetCursorPos(cursor.x + (random() % 3 - 1) * (random() % (runtime / 2200 + 2)), cursor.y + (random() % 3 - 1) * (random() % (runtime / 2200 + 2)));
+
+	out: return 2;
+}
